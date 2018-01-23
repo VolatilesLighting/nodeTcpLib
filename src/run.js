c89fb02ab189688530ef8b809cad843f4751cdb3
flomair <f.mair@gmx.net>
2018-01-23 17:34:03 +0100

    fix tcp

--- src/run.js
+++ src/run.js
@@ -1,36 +1,49 @@
-import vola from './tcp';
+import tcp from '../lib/com/surfaceCom/tcpsend'
+const v = {play_scene: {
+    scene_id: 25,
+    //speed : 10,
+    //brightness: 50,
+    //color_temperature: 700,
+    //color:[0,0,255,0],
+    // set_overlay:0
+}}
+tt1('192.168.1.239',v)
 
 
-function logger(r) {
-    console.log('r',r)
-}
-//upl()
-//tcp()
 
-function tcp() {
-    vola.tcp({
-        ip: '192.168.2.164',
-        cmd: 'get_sec_mode',
-        key: '10a58869d74be5a374cf867cfb473859',
-        keyType: 'master',
-        settings: {timeout: 2000, log: true}
+
+function tt1(ip,v){
+    //console.log('go')
+    //tcp('192.168.2.160', {get_pub_info: null}, false, '', {}, {
+    // tcp('192.168.2.164', {get_surface_info: null}, false, '', {}, {
+    // tcp('192.168.2.159', {set_overlay: null}, false, '', {}, {
+    tcp(ip, v, false, '', {}, {
+        //tcp(ip, {get_installed_scenes: null}, false, '', {}, {
+        //tcp('192.168.2.160', {get_pub_info: null}, false, 'master', {}, {
+        "decryptionRetrys" : 3,
+        "timeout": 5000,
+        "retrys": 50,
+        "delay": 500,
+        log:false
+    }, null).then(data =>{
+           console.log(data)
+        "use strict";
+        if(!Array.isArray(data)) {
+            return;
+            data.forEach(d => {
+                // console.log('./cli.sh 192.168.2.160 rm '+d+'.scn')
+                console.log(d)
+            });
+        }
+        else {
+            console.log(data)
+        }
+        //   return
+        //  data.forEach(d =>{
+
+        //console.log(d,d.adler32,d)
+        //})
+    }).catch(e =>{
+        console.log(e)
     })
-        .then(r => console.log(r))
-        .catch(r => console.log(r))
-}
-function upl() {
-    vola.uploadToSurface({
-        filename: './playground/15',
-        type: 'scene',
-        key: '10a58869d74be5a374cf867cfb473859',
-        ip: '192.168.2.164',
-       // cb: logger
-    })
-        .then(r => console.log(r))
-        .catch(r => console.log(r))
-}
-function disc() {
-    vola.discoverSurfaces('192.168.1.255')
-        .then(r => console.log(r))
-        .catch(r => console.log(r))
 }
